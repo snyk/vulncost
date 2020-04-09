70ec9c67a7b34998e92ab15934128f856626e49e
bmvermeer <brianvermeer@snyk.io>
2020-04-09 10:06:11 +0200

    display vulns in package.json

--- src/getImports/parser.js
+++ src/getImports/parser.js
@@ -1,9 +1,11 @@
 import { getPackages as getPackagesFromJS } from './babelParser';
 import { getPackages as getPackagesFromHTML } from './htmlParser';
+import { getPackages as getPackagesFromPJSON } from './packageJsonParser';
 
 export const TYPESCRIPT = 'typescript';
 export const JAVASCRIPT = 'javascript';
 export const HTML = 'html';
+export const PJSON = 'json';
 
 export function getPackages(fileName, source, language) {
   if ([TYPESCRIPT, JAVASCRIPT].includes(language)) {
@@ -11,6 +13,8 @@ export function getPackages(fileName, source, language) {
   } else if ([HTML].includes(language)) {
     // could be === but maybe we'll expand?
     return getPackagesFromHTML(fileName, source);
+  } else if ([PJSON].includes(language)) {
+    return getPackagesFromPJSON(fileName, source);
   } else {
     return [];
   }
