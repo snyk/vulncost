20783247e02113af7733a532480207f7de9a1cf3
Luke Watts <luke@thisis.la>
2020-04-10 12:01:47 +0200

    test: adds tests for html parser
    I would like to extend the html parser to support a new CDN but worried
    about introducing a regression I am adding some tests to validate the current behaviour

---
+++ test/getImports/htmlParser.test.js
@@ -0,0 +1,52 @@
+import { getPackages } from '../../src/getImports/htmlParser';
+
+const testScenarios = [
+  {
+    name: 'jQuery',
+    url: 'https://code.jquery.com/jquery-3.3.1.min.js',
+    package: 'jquery',
+    version: '3.3.1',
+  },
+  {
+    name: 'Stackpath',
+    url:
+      'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
+    package: 'bootstrap',
+    version: '4.4.1',
+  },
+  {
+    name: 'MaxCDN',
+    url: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+    package: 'bootstrap',
+    version: '3.3.7',
+  },
+  {
+    name: 'unpkg.com',
+    url: 'https://unpkg.com/jquery@3.3.1/dist/jquery.js',
+    package: 'jquery',
+    version: '3.3.1',
+  },
+  {
+    name: 'yandex',
+    url: 'https://yastatic.net/jquery/3.3.1/jquery.min.js',
+    package: 'jquery',
+    version: '3.3.1',
+  },
+  {
+    name: 'jsDeliver – npm',
+    url: 'https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js',
+    package: 'jquery',
+    version: '3.2.1',
+  },
+];
+
+testScenarios.map(scenario => {
+  test(`Extract ${scenario.name} package and version`, () => {
+    const packages = getPackages(
+      'foo',
+      `<script src="${scenario.url}"></script>`
+    );
+    expect(packages[0].name).toBe(scenario.package);
+    expect(packages[0].version).toBe(scenario.version);
+  });
+});
